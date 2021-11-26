5efa798ef000dadb76bd0432cb97643691b0a695
flamber78 <92256515+flamber78@users.noreply.github.com>
2021-11-26 16:17:51 +0100

    Update chatbot.clj

--- chatbot_dcf/chatbot.clj
+++ chatbot_dcf/chatbot.clj
@@ -13,26 +13,31 @@
 (defn not_understand []
   (println "Can you retry please I didn't understand"))
 
-
+;;The exit function that will end the program
 (defn exit-end []
   (println "Do really want to leave ?")
-  (let [x (read-line)]
+  (let [w (read-line)]
     (cond
-      (contains? Positif x) (println "Ok, have a good day and I hope I will see you soon !"))))
+      (contains? Positif w) (println "Ok, if you want more information restart the chatbot or, have a good day and I hope I will see you soon !"))))
 
+
+
+;;Function that will ask wich activity the user wants to do
 (defn activity []
   (println "What do you want to do today ?")
-  (let  [a (read-line)]
+  (let  [v (read-line)]
     (cond
-      (contains? theactivity y) (println "Ok I recommand you to go at a park to do that")
-      (contains? exit y) (exit-end))))
+      (contains? theactivity v) (println "Ok I recommand you to go at a park to do that")
+      (contains? exit v) (exit-end))))
 
 
 (defn Joke []
   (println "Do you want to do an Iron-man?")
   (def joke (read-line))
-  (println "HAHA it was just a joke don't panic")
+  (println "HAHA it was just a joke don't panic"))
 
+
+;;function that will ask to choose from wich park the user wants more information
 (defn  choice_park []
   (println "I know 12 parks in Prague: 
                      1) Vysehrad
@@ -48,27 +53,28 @@
                     11) Frantiskanska Zahrada
                     12) Bertramka"))
 
-
-  (defn park_activity []
+;;This function will return an answer of all the activity for each park
+  (defn ParkActivities []
     (choice_park)
-    let [c (read-line)]
-    (cond
-      (contains? pragueparks c) 
-      (contains? exit c) (exit-end)
-      :else (not_understand)
-    (cond
-      (c = "bertramka") (println "The activities you can do in this park are: walk, biking, parking, attraction like cultural monument, classical music concerts, social events, W. A. ​​Mozart Museum and trams No. 4, 7, 9, 10, 58, 59")
-      (c = "frantiskanska-zahrada") (println "The activities you can do in this park are: walk, biking, playground, wc, attraction like Church of Our Lady of the Snows and Václavské náměstí trams. No. 3, 9, 14, 24, 51, 52, 54, 55, 56, 58, metro A, B Můstek")
-      (c = "obora-hvezda") (println "The activities you can do in this park are: walk, biking, skiing, playground, dogs, parking, wc, attraction like memorial trees, nature trail, nature attractions and Nad Markétou, bus no. 179, 184, 191, 510, Libocká - bus no. 179, 510, Vypich, tram no. 15, 22, 25, 57")
-      (c = "kampa") (println "The activities you can do in this park are: walk, biking, skating, playground, wc, attraction like view of the Vltava, Sovovy Mlýny Gallery, Čertovka, Charles Bridge, Werich House, memorial trees and Hellichova tram no. 12, 20, 22, 23, 57, Újezd ​​tram no. 6, 9, 22, 23, 57, 58, 59")
-      (c = "kinskeho-zahrada") (println "The activities you can do in this park are: walk, playground, parking, wc, attraction like museum,ornamental lakes, natural attraction - Petrin Rocks and Kobrova - BUS No. 176")
-      (c = "klamovka") (println "The activities you can do in this park are: walk, biking, sport, dogs, restaurant, playground, wc, attraction like art gallery, historical attractions, music club and Klamovka - BUS No. 149, 191, 217, tram No. 4, 7, 9, 10, 58, 59")
-      (c = "ladronka") (println "The activities you can do in this park are: walk, biking, parking, skating, restaurant, playground, dogs, wc, attraction like extensive leisure area, music and entertainment events and U Ladronky, Rozýnova, Štefkova BUS No. 191, Vypich BUS No. 108, 174, 180, 191, I3, tram No. 15, 22, 25, 57")
-      (c = "letna") (println "The activities you can do in this park are: walk, biking, playground, dogs, restaurant, skating, parking, wc, attraction like extensive leisure area, cultural events and Letenské náměstí and Sparta tram no. 1, 8, 15, 25, 26, 51, 56, Čechův most tram no. 12, 17, 53, Chotkovy sady tram no. 18, 20, 57, metro Hradčanská")
-      (c = "petrin") (println "The activities you can do in this park are: walk, biking, parking, playground, restaurant, skating, wc,attraction like lookout tower, maze, observatory, underground galleries and fruit orchards and Újezd ​​tram no. 6, 9, 12, 20, 22, 23, 57, 58, 59, Hellichova tram no. 12, 20, 22, 23, 57, Pohořelec tram no. 22, 23, Dormitories Strahov BUS No. 143, 149, 217")
-      (c = "riegrovy-sady") (println "The activities you can do in this park are: walk, biking, dogs, playground, restaurant, sport, wc attraction like view of Prague, classicist lookout tower, three-sided obelisk and Metro Jiřího z Poděbrad, Na Smetance Bus no. 135, Italská and Vinohradská tržnice tram no. 11")
-      (c = "stromovka") (println "The activities you can do in this park are: walk, biking, skating, sport, parking, playgorund, dogs, restaurant, wc attraction like zoo, botanical gardens, natural attractions, planetarium, exhibition grounds and Výstaviště, tram no. 5, 12, 14, 15, 17, 53, 54, Sibiřské nám., Bus no. 131, by steamer from Rašínov nábř")
-      (c = "vysehrad") (println "The activities you can do in this park are: walk, biking, parking, sport, playground, wc, attraction like sightseeing, museum, cemetery of famous personalities Slavín, national cultural monument and Vyšehrad - metro C, Podolská vodárna tram no. 3, 16, 17, 21, 52"))))
+    (let [c (read-line)]
+      (cond
+        (contains? pragueparks c)(println "So" c)
+        (contains? exit c) (exit-end)
+        :else (not_understand))
+      (cond
+      (= c "bertramka") (println "the activities you can do in this park are: walk, biking, parking, attraction like cultural monument, classical music concerts, social events, W. A. ​​Mozart Museum and trams No. 4, 7, 9, 10, 58, 59")
+      (= c "frantiskanska-zahrada") (println "the activities you can do in this park are: walk, biking, playground, wc, attraction like Church of Our Lady of the Snows and Václavské náměstí trams. No. 3, 9, 14, 24, 51, 52, 54, 55, 56, 58, metro A, B Můstek")
+      (= c "obora-hvezda") (println "the activities you can do in this park are: walk, biking, skiing, playground, dogs, parking, wc, attraction like memorial trees, nature trail, nature attractions and Nad Markétou, bus no. 179, 184, 191, 510, Libocká - bus no. 179, 510, Vypich, tram no. 15, 22, 25, 57")
+      (= c "kampa") (println "the activities you can do in this park are: walk, biking, skating, playground, wc, attraction like view of the Vltava, Sovovy Mlýny Gallery, Čertovka, Charles Bridge, Werich House, memorial trees and Hellichova tram no. 12, 20, 22, 23, 57, Újezd ​​tram no. 6, 9, 22, 23, 57, 58, 59")
+      (= c "kinskeho-zahrada") (println "the activities you can do in this park are: walk, playground, parking, wc, attraction like museum,ornamental lakes, natural attraction - Petrin Rocks and Kobrova - BUS No. 176")
+      (= c "klamovka") (println "the activities you can do in this park are: walk, biking, sport, dogs, restaurant, playground, wc, attraction like art gallery, historical attractions, music club and Klamovka - BUS No. 149, 191, 217, tram No. 4, 7, 9, 10, 58, 59")
+      (= c "ladronka") (println "the activities you can do in this park are: walk, biking, parking, skating, restaurant, playground, dogs, wc, attraction like extensive leisure area, music and entertainment events and U Ladronky, Rozýnova, Štefkova BUS No. 191, Vypich BUS No. 108, 174, 180, 191, I3, tram No. 15, 22, 25, 57")
+      (= c "letna") (println "the activities you can do in this park are: walk, biking, playground, dogs, restaurant, skating, parking, wc, attraction like extensive leisure area, cultural events and Letenské náměstí and Sparta tram no. 1, 8, 15, 25, 26, 51, 56, Čechův most tram no. 12, 17, 53, Chotkovy sady tram no. 18, 20, 57, metro Hradčanská")
+      (= c "petrin") (println "the activities you can do in this park are: walk, biking, parking, playground, restaurant, skating, wc,attraction like lookout tower, maze, observatory, underground galleries and fruit orchards and Újezd ​​tram no. 6, 9, 12, 20, 22, 23, 57, 58, 59, Hellichova tram no. 12, 20, 22, 23, 57, Pohořelec tram no. 22, 23, Dormitories Strahov BUS No. 143, 149, 217")
+      (= c "riegrovy-sady") (println "the activities you can do in this park are: walk, biking, dogs, playground, restaurant, sport, wc attraction like view of Prague, classicist lookout tower, three-sided obelisk and Metro Jiřího z Poděbrad, Na Smetance Bus no. 135, Italská and Vinohradská tržnice tram no. 11")
+      (= c "stromovka") (println "the activities you can do in this park are: walk, biking, skating, sport, parking, playgorund, dogs, restaurant, wc attraction like zoo, botanical gardens, natural attractions, planetarium, exhibition grounds and Výstaviště, tram no. 5, 12, 14, 15, 17, 53, 54, Sibiřské nám., Bus no. 131, by steamer from Rašínov nábř")
+      (= c "vysehrad") (println "the activities you can do in this park are: walk, biking, parking, sport, playground, wc, attraction like sightseeing, museum, cemetery of famous personalities Slavín, national cultural monument and Vyšehrad - metro C, Podolská vodárna tram no. 3, 16, 17, 21, 52"))))
+    
     
 ;;Main programm
 
@@ -79,7 +85,7 @@
     (println (str "Hi  " surname "  nice to meet you"))
     (Joke)
     (activity)
-    (park_activity)
+    (ParkActivities)
     (exit-end))
   
   ;;End
